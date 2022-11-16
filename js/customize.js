273d02a47a84c0011ec315e8623eb3093e4514ad
Miley <miley.ho@hyweb.com.tw>
2022-11-16 15:17:11 +0800

    update

--- js/customize.js
+++ js/customize.js
@@ -75,28 +75,84 @@ accordionSlider({
     });
 
     //分類瀏覽輪播
-    const npSwiper = new Swiper('.npSlider .swiper', {
+    const lpSwiper_1 = new Swiper('.lpSlider_1 .swiper', {
         slidesPerView: 5,
         spaceBetween: 30,
         loop: false,
         // 切換點
         pagination: {
-            el: '.adSlider .swiper-dots',
+            el: '.lpSlider_1 .swiper-dots',
             bulletElement: 'button',
             clickable: true,
         },
         // 切換箭頭
         navigation: {
-            nextEl: '.npSlider .swiperArrow.next', //自行設定樣式
-            prevEl: '.npSlider .swiperArrow.prev', //自行設定樣式
-            disabledClass: '.npSlider swiperArrow-disabled', //不可點選樣式
+            nextEl: '.lpSlider_1 .swiperArrow.next', //自行設定樣式
+            prevEl: '.lpSlider_1 .swiperArrow.prev', //自行設定樣式
+            disabledClass: '.lpSlider_1 swiperArrow-disabled', //不可點選樣式
         },
         breakpoints: {
             100: {
-                slidesPerView: 2,
+                slidesPerView: 1,
             },
             767: {
-                slidesPerView: 3,
+                slidesPerView: 2,
+            },
+            1000: {
+                slidesPerView: 4,
+            },
+        },
+    });
+    const lpSwiper_2 = new Swiper('.lpSlider_2 .swiper', {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        loop: false,
+        // 切換點
+        pagination: {
+            el: '.lpSlider_2 .swiper-dots',
+            bulletElement: 'button',
+            clickable: true,
+        },
+        // 切換箭頭
+        navigation: {
+            nextEl: '.lpSlider_2 .swiperArrow.next', //自行設定樣式
+            prevEl: '.lpSlider_2 .swiperArrow.prev', //自行設定樣式
+            disabledClass: '.lpSlider_2 swiperArrow-disabled', //不可點選樣式
+        },
+        breakpoints: {
+            100: {
+                slidesPerView: 1,
+            },
+            767: {
+                slidesPerView: 2,
+            },
+            1000: {
+                slidesPerView: 4,
+            },
+        },
+    });
+    const lpSwiper_3 = new Swiper('.lpSlider_3 .swiper', {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        loop: false,
+        // 切換點
+        pagination: {
+            el: '.lpSlider_3 .swiper-dots',
+            bulletElement: 'button',
+            clickable: true,
+        },
+        // 切換箭頭
+        navigation: {
+            nextEl: '.lpSlider_3 .swiperArrow.next', //自行設定樣式
+            prevEl: '.lpSlider_3 .swiperArrow.prev', //自行設定樣式
+            disabledClass: '.lpSlider_3 swiperArrow-disabled', //不可點選樣式
+        },
+        breakpoints: {
+            100: {
+                slidesPerView: 1,
+            },
+            767: {
+                slidesPerView: 2,
             },
             1000: {
                 slidesPerView: 4,
@@ -143,6 +199,35 @@ accordionSlider({
             nextEl: '.marquee .marquee-arrow.marquee-next', //自行設定樣式
             prevEl: '.marquee .marquee-arrow.marquee-prev', //自行設定樣式
             disabledClass: '.marquee marquee-arrow-disabled', //不可點選樣式
+        },
+    });
+    //cp_photo
+    const navSlider = new Swiper('.navSlider .swiper', {
+        lazy: true, // lazy load
+        preloadImages: false, // 多筆設定lazy時須設定
+        centeredSlides: false, // 多筆設定lazy時須設定
+        slidesPerView: 4,
+        // watchSlidesProgress: true,
+        navigation: {
+            nextEl: '.navSlider .nextSlider', //下一張class，無障礙設定關係需要增加.nextSlider
+            prevEl: '.navSlider .prevSlider', //前一張class，無障礙設定關係需要增加.prevSlider
+            disabledClass: 'swiperArrow-disabled', //不可點選樣式
+        },
+    });
+
+    const sliderFor = new Swiper('.sliderFor .swiper', {
+        slidesPerView: 1, //顯示張數
+        effect: 'fade', //淡入
+        fadeEffect: {
+            crossFade: true, //上一張淡出，false上一張不淡出，下一張疊在上方
+        },
+        pagination: {
+            el: '.sliderFor .pagination',
+            type: 'fraction', //顯示分頁
+        },
+        lazy: true,
+        thumbs: {
+            swiper: navSlider, //設定指向到哪個swiper，使用另一個設定的參數
         },
     });
 })();
